943fcff72ef162badcfaeb2ea34cd7a9432bb659
Iain Sandoe <iain@sandoe.co.uk>
2025-01-27 07:16:26 +0000

    c++, contracts, Darwin: Fix local symbols for linker.
    Darwin partitions objects into 'atoms' based on linker-visible
    symbols.  If these are not present, and weak and non-weak sections
    overlap we get spurious warnings.  We need the contract violation
    constants and the source locations to be visible.
    
    In addition, this fixes those symbols such that we do not
    unconditionally emit periods into the names (some assemblers do
    not accept this).
    
    TODO: fix other cases at some stage.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/cp/contracts.cc
+++ gcc/cp/contracts.cc
@@ -2016,7 +2016,7 @@ static tree
 build_contracts_source_location (location_t loc)
 {
   tree impl_ = contracts_tu_local_named_var
-    (loc, "Lsrc_loc_impl.",
+    (loc, "Lsrc_loc_impl",
      get_contracts_source_location_impl_type (), /*is_const*/true);
 
   DECL_INITIAL (impl_) = build_contracts_source_location_impl (loc);
@@ -2272,7 +2272,7 @@ build_contract_violation_P2900 (tree contract, bool is_const)
   TREE_STATIC (ctor) = true;
 
   tree viol_ = contracts_tu_local_named_var
-    (EXPR_LOCATION (contract), "Lcontract_violation.",
+    (EXPR_LOCATION (contract), "Lcontract_violation",
      get_pseudo_contract_violation_type (), /*is_const*/true);
 
   TREE_CONSTANT (viol_) = is_const;
