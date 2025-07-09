f6d95a41d4c7fe1d5bc8bdbc148123e8b4a7d65f
Iain Sandoe <iain@sandoe.co.uk>
2025-07-09 12:23:09 +0100

    c++, contracts: Allow for dynamic contract stmt content.
    This allows us to put values for semantic and assertion type that will
    not fit into the LANG_ bits
    
    This is intended to allow the library to make use of compiler-side logic
    to implement changes in semantic handling, rather than duplicating this.

--- gcc/cp/contracts.h
+++ gcc/cp/contracts.h
@@ -252,6 +252,14 @@ enum contract_inheritance
   CONTRACTS_ON_VIRTUALS_P3653 = 2,
 };
 
+/* P2900 meanings.  */
+/* evaluation_semantic */
+#define CONTRACT_EVALUATION_SEMANTIC(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 0))
+
+#define CONTRACT_ASSERTION_KIND(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 1))
+
 /* =========================== common ============================  */
 
 #define CONTRACT_CHECK(NODE) \
@@ -304,7 +312,7 @@ enum contract_inheritance
 
 /* The parsed condition of the contract.  */
 #define CONTRACT_CONDITION(NODE) \
-  (TREE_OPERAND (CONTRACT_CHECK (NODE), 1))
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 2))
 
 /* True iff the condition of the contract NODE is not yet parsed.  */
 #define CONTRACT_CONDITION_DEFERRED_P(NODE) \
@@ -312,12 +320,16 @@ enum contract_inheritance
 
 /* The raw comment of the contract.  */
 #define CONTRACT_COMMENT(NODE) \
-  (TREE_OPERAND (CONTRACT_CHECK (NODE), 2))
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 3))
+
+/* A std::source_location, if provided.  */
+#define CONTRACT_STD_SOURCE_LOC(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 4))
 
 /* The VAR_DECL of a postcondition result. For deferred contracts, this
    is an IDENTIFIER.  */
 #define POSTCONDITION_IDENTIFIER(NODE) \
-  (TREE_OPERAND (POSTCONDITION_STMT_CHECK (NODE), 3))
+  (TREE_OPERAND (POSTCONDITION_STMT_CHECK (NODE), 5))
 
 /* For a FUNCTION_DECL of a guarded function, this holds the function decl
    where pre contract checks are emitted.  */
