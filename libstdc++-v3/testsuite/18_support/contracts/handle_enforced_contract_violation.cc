34be9940c216f2ca1fc2c4253b193423344bf8eb
Nina Ranns <dinka.ranns@gmail.com>
2025-07-17 12:59:16 +0100

    more P3290 fixes
    removing spurious diagnostic for #include_next
    adding description of tests
    fixing a test that wasn't testing what it was intending to test

--- libstdc++-v3/testsuite/18_support/contracts/handle_enforced_contract_violation.cc
+++ libstdc++-v3/testsuite/18_support/contracts/handle_enforced_contract_violation.cc
@@ -15,6 +15,8 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
+// Check that a case with user provided contract violation handler works as expected
+// Semantic chosen is a non terminating one.
 // { dg-options "-g0 -fcontracts -fcontracts-nonattr -fcontract-evaluation-semantic=observe" }
 // { dg-do run { target c++2a } }
 
