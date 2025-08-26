b69d6f65fa110f8ec1dae6b8ff06625834450259
Iain Sandoe <iain@sandoe.co.uk>
2025-08-26 05:55:49 +0100

    c++, contracts: Default to inlined contract checks.
    We still have the option to outline them with -fcontract-checks-outlined.
    
    Switch the tests that were checking -fno-contract-checks-outlined to do the
    opposite.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/g++.dg/contracts/cpp26/outline-checks/freefunc-noexcept-pre.C
+++ gcc/testsuite/g++.dg/contracts/cpp26/outline-checks/freefunc-noexcept-pre.C
@@ -1,8 +1,8 @@
 // Throwing violation handler in a pre/post check on a noexcept function
 // behaves as if the function exited via an exception.
-// This tests the behaviour of a post condition on a member function
+// This tests the behaviour of a pre condition on a member function
 // { dg-do run }
-// { dg-options "-std=c++2a -fcontracts -fcontracts-nonattr -fcontract-evaluation-semantic=observe -fno-contract-checks-outlined" }
+// { dg-options "-std=c++2a -fcontracts -fcontracts-nonattr -fcontract-evaluation-semantic=observe -fcontract-checks-outlined" }
 
 #include <contracts>
 #include <exception>
@@ -23,24 +23,21 @@ void handle_contract_violation(const std::contracts::contract_violation& violati
   throw MyException{};
 }
 
-struct X
+void f(int x) noexcept pre(x>1)
 {
-    void f(const int x) noexcept post(x>1) {
-      try{
-       int i = 1;
-      }
-      catch(...) {
-      }
-    }
-};
+  try{
+   int i = 1;
+  }
+  catch(...) {
+  }
+}
 
 int main()
 {
   std::set_terminate (my_term);
   try
   {
-      X x;
-      x.f(-42);
+      f(-42);
   } catch (...) {
   }
   // We should not get here
